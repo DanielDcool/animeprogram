2bf9d5cf20ef0d843bee226c954149cf0c1e17b8
daniel <daniel@lemonla.net>
2026-08-15 01:49:57 +0900

    feat(server): 新增 TMDB 日剧目录客户端

---
+++ server/src/modules/drama/editorial.ts
@@ -0,0 +1,8 @@
+export interface DramaEditorialNote {
+  badge: string;
+  reason: string;
+}
+
+export function dramaEditorialNote(_id: number): DramaEditorialNote | undefined {
+  return undefined;
+}
